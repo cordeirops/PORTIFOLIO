f367d9cb0ee2f3c7c5205ee3f70d06178685972b
ps_coLinux <pedro.sbarainicordeiro@gmail.com>
2023-11-08 15:23:39 -0300

    Final

--- Scripts/script.js
+++ Scripts/script.js
@@ -2,75 +2,116 @@ const form = document.getElementById('form');
 const campos = document.querySelectorAll('.required');
 const spans = document.querySelectorAll('.span-required');
 const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+const modal = document.getElementById('Modal');
+const backdrop = document.getElementById('backdrop');
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    nameValidate();
-    emailValidate();
-    telefoneValidate();
+// Adicione um ouvinte de evento ao backdrop (elemento de fundo) para fechar o modal
+backdrop.addEventListener("click", () => {
+    closeModal();
 });
 
+// Função para verificar se há erros nos campos
+function hasErrors() {
+    return Array.from(campos).some((campo) => campo.style.border);
+}
+
+// Ouvinte de evento para redefinir campos
 form.addEventListener('reset', () => {
     campos.forEach((campo, index) => {
-        campo.style.border = ''; // Remove a borda vermelha
-        spans[index].style.display = 'none'; // Oculta os spans de validação
+        removeError(index);
     });
 });
 
-
+// Função para definir um erro em um campo
 function setError(index) {
     campos[index].style.border = '2px solid #e63636';
     spans[index].style.display = 'block';
 }
 
+// Função para remover um erro de um campo
 function removeError(index) {
     campos[index].style.border = '';
     spans[index].style.display = 'none';
 }
 
+// Função para validar o nome
 function nameValidate() {
-    if (campos[0].value.length < 3) {
+    const nome = campos[0].value;
+    if (nome.length < 3) {
         setError(0);
     } else {
         removeError(0);
     }
 }
 
+// Função para validar o email
 function emailValidate() {
-    if (!emailRegex.test(campos[1].value)) {
+    const email = campos[1].value;
+    if (!emailRegex.test(email)) {
         setError(1);
     } else {
         removeError(1);
     }
 }
 
+// Função para validar o telefone
 function telefoneValidate() {
-    let telefoneInput = campos[2];
+    const telefoneInput = campos[2];
     let value = telefoneInput.value.replace(/\D/g, '');
 
     if (telefoneInput.value === 'Digite seu telefone') {
         telefoneInput.value = '';
     }
 
-    if (value.length === 0) {
-        setError(2);
-        spans[2].style.display = 'block';
-    } else if (value.length !== 11) {
+    if (value.length === 0 || value.length !== 11) {
         setError(2);
         spans[2].textContent = 'Telefone deve ter 11 dígitos';
         spans[2].style.display = 'block';
     } else {
         telefoneInput.value = `(${value.slice(0, 2)}) ${value.slice(2, 7)}-${value.slice(7, 11)}`;
         removeError(2);
-        spans[2].style.display = 'none';
     }
 }
 
-campos[2].addEventListener('focus', function() {
+// Ouvinte de evento para o campo de telefone ao receber foco
+campos[2].addEventListener('focus', function () {
     if (campos[2].value === 'Digite seu telefone') {
         campos[2].value = '';
     }
 });
 
+// Função para abrir o modal
+function openModal() {
+    modal.style.display = "flex";
+}
 
+// Função para fechar o modal
+function closeModal() {
+    console.log('Fechando o modal'); // Adicione esta linha
+    modal.style.display = 'none';
+}
 
+// Adicione o ouvinte de evento ao formulário para prevenir a submissão padrão
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    nameValidate();
+    emailValidate();
+    telefoneValidate();
+
+    if (!hasErrors()) {
+        const formData = {
+            nome: campos[0].value,
+            email: campos[1].value,
+            telefone: campos[2].value,
+            descricao: document.getElementById('descricao').value,
+        };
+
+        // Preencha os dados no modal de sucesso
+        document.getElementById('modal-nome').textContent = formData.nome;
+        document.getElementById('modal-email').textContent = formData.email;
+        document.getElementById('modal-telefone').textContent = formData.telefone;
+        document.getElementById('modal-descricao').textContent = formData.descricao;
+
+        openModal();
+    }
+});
